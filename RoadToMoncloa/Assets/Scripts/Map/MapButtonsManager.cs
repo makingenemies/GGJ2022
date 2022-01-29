e422b1b43b6cc06ff0f8d560191c7c8029f99ec7
albpb2 <albpb2@gmail.com>
2022-01-29 23:22:40 +0100

    Enable only current level button in map

---
+++ RoadToMoncloa/Assets/Scripts/Map/MapButtonsManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapButtonsManager : MonoBehaviour
+{
+    [SerializeField] private Button[] _buttons;
+
+    private GameState _gameState;
+
+    void Start()
+    {
+        _gameState = FindObjectOfType<GameState>();
+        SetUpButtons();
+    }
+
+    private void SetUpButtons()
+    {
+        DisableAllButtons();
+        EnableCurrentLevelButton();
+    }
+
+    private void DisableAllButtons()
+    {
+        foreach (var button in _buttons)
+        {
+            button.interactable = false;
+        }
+    }
+
+    private void EnableCurrentLevelButton()
+    {
+        _buttons[_gameState.CurrentLevelIndex].interactable = true;
+    }
+}
