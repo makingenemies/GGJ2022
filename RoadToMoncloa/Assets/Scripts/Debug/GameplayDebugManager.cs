ebec9c7ad96a63f742602e802fe7697f3df8bee3
albpb2 <albpb2@gmail.com>
2022-01-29 12:08:52 +0100

    Add debug commands to set money and voters

---
+++ RoadToMoncloa/Assets/Scripts/Debug/GameplayDebugManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class GameplayDebugManager : MonoBehaviour
+{
+    [SerializeField] private TMPro.TMP_InputField _moneyAmountInputField;
+    [SerializeField] private TMPro.TMP_InputField _votersCountInputField;
+
+    private MoneyCounter _moneyCounter;
+    private VotersCounter _votersCounter;
+
+    private void Start()
+    {
+        _moneyCounter = FindObjectOfType<MoneyCounter>();
+        _votersCounter = FindObjectOfType<VotersCounter>();
+    }
+
+    public void SetMoneyAmount()
+    {
+        var delta = int.Parse(_moneyAmountInputField.text) - _moneyCounter.CurrentAmount;
+        _moneyCounter.UpdateCurrentAmount(delta);
+    }
+
+    public void SetVotersCount()
+    {
+        var delta = int.Parse(_votersCountInputField.text) - _votersCounter.CurrentAmount;
+        _votersCounter.UpdateCurrentAmount(delta);
+    }
+}
