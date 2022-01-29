a8b4a8498d5f04275fcb3aa2da8f9cc02392b458
albpb2 <albpb2@gmail.com>
2022-01-29 01:56:03 +0100

    Enable and disable donate button basing on lies counter

--- RoadToMoncloa/Assets/Scripts/Gameplay/DonationManager.cs
+++ RoadToMoncloa/Assets/Scripts/Gameplay/DonationManager.cs
@@ -1,16 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class DonationManager : MonoBehaviour
+public class DonationManager : MonoBehaviour, IEventHandler<LiePlayedEvent>
 {
     [SerializeField] private GameObject _donationPanel;
+    [SerializeField] private Button _openPanelButton;
 
     private MoneyCounter _moneyCounter;
     private LiesManager _liesManager;
+    private EventBus _eventBus;
 
     private void Start()
     {
         _moneyCounter = FindObjectOfType<MoneyCounter>();
         _liesManager = FindObjectOfType<LiesManager>();
+        _eventBus = FindObjectOfType<EventBus>();
+
+        _eventBus.Register(this);
+    }
+
+    private void OnEnable()
+    {
+        if (_eventBus != null)
+        {
+            _eventBus.Register(this);
+        }
+    }
+
+    private void OnDisable()
+    {
+        _eventBus.Unregister(this);
     }
 
     public void ShowDonationPanel()
@@ -27,6 +46,16 @@ public class DonationManager : MonoBehaviour
     {
         _moneyCounter.UpdateCurrentAmount(-donationMoneyAmount);
         _liesManager.ResetPlayedLies();
+
         _donationPanel.SetActive(false);
+        _openPanelButton.interactable = false;
+    }
+
+    public void HandleEvent(LiePlayedEvent @event)
+    {
+        if (@event.IsLiesCounterFull)
+        {
+            _openPanelButton.interactable = true;
+        }
     }
 }
