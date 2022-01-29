16294ab6310a8ad56bdab6b5f1881123445a95b9
albpb2 <albpb2@gmail.com>
2022-01-29 10:44:15 +0100

    Add pause menu without functionality

--- RoadToMoncloa/Assets/Scripts/Gameplay/PauseManager.cs
+++ RoadToMoncloa/Assets/Scripts/Gameplay/PauseManager.cs
@@ -2,9 +2,36 @@
 
 public class PauseManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _pausePanel;
+    [SerializeField] private GameObject _debugCommandsPausePanel;
+
     private bool _isPaused;
 
     public bool IsPaused => _isPaused;
+
+    private void Start()
+    {
+#if !UNITY_EDITOR
+        _debugCommandsPausePanel.setActive(false);
+#endif
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!IsPaused)
+            {
+                Pause();
+                _pausePanel.SetActive(true);
+            }
+            else
+            {
+                Unpause();
+                _pausePanel.SetActive(false);
+            }
+        }
+    }
 
     public void Pause()
     {
