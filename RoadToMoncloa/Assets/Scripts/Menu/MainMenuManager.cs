bc0f0ed90aad264396042acebb3564924b79da1d
albpb2 <albpb2@gmail.com>
2022-01-30 01:24:51 +0100

    Add exit button

---
+++ RoadToMoncloa/Assets/Scripts/Menu/MainMenuManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    [SerializeField] private Button _exitButton;
+
+    private void Start()
+    {
+#if UNITY_WEBGL && !UNITY_EDITOR
+        _exitButton.gameObject.SetActive(false);
+#endif
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+}
