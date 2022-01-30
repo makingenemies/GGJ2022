eb393bbb97928f330e9b6756002831ebd1fd5320
albpb2 <albpb2@gmail.com>
2022-01-30 12:29:37 +0100

    Use variable gameplay backgrounds

---
+++ RoadToMoncloa/Assets/Scripts/Gameplay/GameplayBackgroundManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class GameplayBackgroundManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] _leftZoneBackgroundPrefabs;
+    [SerializeField] private GameObject[] _rightZoneBackgroundPrefabs;
+    [SerializeField] private GameObject _leftZoneParent;
+    [SerializeField] private GameObject _rightZoneParent;
+
+    private void Start()
+    {
+        var gameState = FindObjectOfType<GameState>();
+        Instantiate(_leftZoneBackgroundPrefabs[gameState.CurrentLevelIndex], _leftZoneParent.transform);
+        Instantiate(_rightZoneBackgroundPrefabs[gameState.CurrentLevelIndex], _rightZoneParent.transform);
+    }
+}
