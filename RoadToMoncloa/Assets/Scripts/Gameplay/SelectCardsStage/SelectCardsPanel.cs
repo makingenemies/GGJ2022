5378cdd2937cd049f0784ba969d892d518bf2328
albpb2 <albpb2@gmail.com>
2023-01-28 11:05:32 +0100

    Create script to manage panel of cards to select

---
+++ RoadToMoncloa/Assets/Scripts/Gameplay/SelectCardsStage/SelectCardsPanel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectCardsPanel : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _cardPlaceHolders;
+
+    public List<Transform> CardPlaceHolders => _cardPlaceHolders;
+
+    public void SetActive(bool active)
+    {
+        gameObject.SetActive(active);
+    }
+}
